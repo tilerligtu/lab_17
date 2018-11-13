34e2fd258eb84d8ec5b6b7849ad6957221f22dbb
Tristan Hipolito <tristan@tristans-imac.san.rr.com>
2018-11-12 18:44:08 -0800

    Initial Commit

---
+++ lab_17/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lab_17
+//
+//  Created by Tristan Hipolito on 11/12/18.
+//  Copyright © 2018 kodi.testkodi.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
